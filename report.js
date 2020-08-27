c4144e35a15f1496e483e1739558c1684822d5d3
feifan zhou <superzhou7@gmail.com>
2020-08-27 22:19:54 +0800

    init

---
+++ report.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+const axios = require('axios');
+const color = require('chalk');
+const cron = require('node-cron');
+const { report } = require('./token_config.json');
+function sendMessage() {
+  axios({
+    method: 'post',
+    // url: `https://oapi.dingtalk.com/robot/send?access_token=${report1}`,
+    url: `https://oapi.dingtalk.com/robot/send?access_token=${report}`,
+    headers: { 'Content-Type': 'application/json' },
+    data: {
+      "msgtype": "text", 
+      "text": {
+        "content": "沃尼玛温馨提示您，写周报啦~~~"
+      }, 
+      "at": {
+        "atMobiles": [
+        ], 
+        "isAtAll": true
+      }
+    },
+  }).then(() => {
+    console.log(color.green('发送成功'));
+  }).catch(error=>{
+    console.log(color.yellow(error));
+  });
+}
+
+cron.schedule('0 17 * * 5', () => {
+  sendMessage();
+});
